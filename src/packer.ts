7f2dad2f41249c56f6cff13d69bd0c08ecd9412c
Emma <EmmaRussellKilburn@gmail.com>
2025-01-23 16:12:17 +0000

    add simple unpack function

--- src/packer.ts
+++ src/packer.ts
@@ -1,3 +1,5 @@
+import * as ndarray from "ndarray";
+
 const preparePackArrayForShape = (name: string, shape: number | number[] | null) => {
     // For a given shape and base name, return packed names, shape and n (number of values after expansion
     const scalar = shape === null || shape === [];
@@ -59,6 +61,7 @@ export interface PackerOptions {
 
 export class Packer {
     private options: Partial<PackerOptions>;
+    private len: number;
     private nms: Array<string>;
     private idx: object;
     private shape: object;
@@ -90,24 +93,64 @@ export class Packer {
             this.nms.push(...scalar);
         }
 
-        let len = scalar?.size || 0;
+        this.len = scalar?.size || 0;
         if (array) {
             for (let [name, value] of array) {
-                // TODO: test for duplicates with scalar
                 const tmp = preparePackArrayForShape(name, value);
                 this.nms.push(...tmp.names);
                 this.shape[name] = tmp.shape;
                 // array of 0-based indexes where each of the named values can be found
-                this.idx[name] = [...Array(tmp.n).keys()].map(i => len + i);
-                len = len + tmp.n;
+                this.idx[name] = [...Array(tmp.n).keys()].map(i => this.len + i);
+                this.len = this.len + tmp.n;
             }
         }
 
-        if (!this.idx.keys().length) {
+        if (!this.len) {
             throw Error("Trying to generate an empty packet. You have not provided any entries in 'scalar' or 'array', " +
                         "which implies generating from a zero-length parameter vector.");
         }
     }
 
-    // Implement unpack first, pack can be throw TODO
+    // Unpack a one-dimensional array
+    public unpack(x: Array<number>) {
+        if (x.length !== this.len) {
+            throw Error(`Incorrect length input; expected ${this.len} but given ${x.length}.`);
+        }
+
+        // Return a map of names to values in the format described by shape
+        let result = new Map<string, any>();
+        for (let name in this.shape) {
+            if (this.shape[name] === [0]) {
+                // scalar
+                const i = this.idx[name] as number;
+                result.set(name, x[i])
+            } else {
+                // array
+                const startIdx = this.idx[name][0];
+                const length = this.idx[name].length;
+                const values = x.slice(startIdx, startIdx + length -1);
+                if (this.shape[name].length == 1) {
+                    // one-dimensional array
+                    result.set(name, values);
+                } else {
+                    // multi-dimensional array
+                    result.set(name, ndarray(values, this.shape[name]))
+                }
+            }
+        }
+
+        const { fixed, process } = this.options;
+
+        if (fixed) {
+            Array(fixed.entries()).forEach((key, value) => {
+                result[key] = value;
+            });
+        }
+
+        if (process) {
+            result = process(result);
+        }
+
+        return result;
+    }
 }
