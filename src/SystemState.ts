fa86fa7b82dc71eac2babd53f561f79b2f5300e8
Emma <EmmaRussellKilburn@gmail.com>
2025-03-17 17:19:49 +0000

    implement reordering in SystemState

--- src/SystemState.ts
+++ src/SystemState.ts
@@ -30,7 +30,7 @@ export interface ParticleState {
  * {@link SystemState.nStateElements | nStateElements} numeric values.
  */
 export class SystemState {
-    private readonly _state: ndarray.NdArray;
+    private _state: ndarray.NdArray;
     private readonly _nGroups: number;
     private readonly _nParticles: number;
     private readonly _nStateElements: number;
@@ -45,9 +45,13 @@ export class SystemState {
         this._nGroups = nGroups;
         this._nParticles = nParticles;
         this._nStateElements = nStateElements;
+        this._state = this.newState();
+    }
+
+    private newState() {
         // arrange the ndArray with dimensions: group, particle, stateElement
-        const len = nGroups * nParticles * nStateElements;
-        this._state = ndarray(new Array<number>(len).fill(0), [nGroups, nParticles, nStateElements]);
+        const len = this._nGroups * this._nParticles * this._nStateElements;
+        return ndarray(new Array<number>(len).fill(0), [this._nGroups, this._nParticles, this._nStateElements]);
     }
 
     /**
@@ -100,5 +104,31 @@ export class SystemState {
     private checkIndexes(iGroup: number, iParticle: number) {
         checkIntegerInRange("Group index", iGroup, 0, this._nGroups - 1);
         checkIntegerInRange("Particle index", iParticle, 0, this._nParticles - 1);
+    }
+
+    /**
+     * Apply a reordering to the particles in every group according to a reordering parameter which defines
+     * a new order for each group.
+     * @param reordering {@link https://github.com/scijs/ndarray | NdArray } whose first dimension defines group index
+     * and whose second dimension defines a new order of particles for each group. For example, if this state has three
+     * particles per group, a reordering of `[2, 0, 1]` for any group would imply that its current third particle should
+     * be reordered to the first position, its current first particle to the second position, and its current second
+     * particle to the third position.
+     */
+    public reorder(reordering: ndarray.NdArray) {
+        // TODO: expect reordering shape to be ngroups * nparticles
+        // TODO: expect each reordering to contain all and only expected values
+        const reordered = this.newState();
+        for (let iGroup = 0; iGroup < this._nGroups; iGroup++) {
+            for (let newParticleIndex = 0; newParticleIndex < this._nParticles; newParticleIndex++) {
+                const oldParticleIndex = reordering.get(iGroup, newParticleIndex);
+                const particleValues = this.getParticle(iGroup, oldParticleIndex);
+                for (let iStateElement = 0; iStateElement < this._nStateElements; iStateElement++) {
+                    reordered.set(iGroup, newParticleIndex, iStateElement, particleValues.get(iStateElement));
+                }
+            }
+        }
+
+        this._state = reordered;
     }
 }
