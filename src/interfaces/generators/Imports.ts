ff826ac6a9ca7da85bb8171992f55bcb8296b2fe
Mantra <mantrakusumgar@outlook.com>
2026-06-09 16:13:17 +0100

    add in io in imports

--- src/interfaces/generators/Imports.ts
+++ src/interfaces/generators/Imports.ts
@@ -1,13 +1,15 @@
 import { Packer } from "../../Packer.ts";
 import { array } from "./imports/array.ts";
 import { interpolate } from "./imports/interpolate.ts";
+import { io } from "./imports/io.ts";
 import { math } from "./imports/math.ts";
 
 export const imports = {
     Packer,
     math,
     array,
-    interpolate
+    interpolate,
+    io
 };
 
 export type Imports = typeof imports;
