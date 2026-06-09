e98218761723a66d6e9768ddb1fe1670aee7a460
Mantra <mantrakusumgar@outlook.com>
2026-06-09 16:05:50 +0100

    add interpolate utilities

--- src/interfaces/generators/Imports.ts
+++ src/interfaces/generators/Imports.ts
@@ -1,11 +1,13 @@
 import { Packer } from "../../Packer.ts";
 import { array } from "./imports/array.ts";
+import { interpolate } from "./imports/interpolate.ts";
 import { math } from "./imports/math.ts";
 
 export const imports = {
     Packer,
     math,
-    array
+    array,
+    interpolate,
 };
 
 export type Imports = typeof imports;
---
+++ src/interfaces/generators/imports/interpolate.ts
@@ -0,0 +1,83 @@
+import { DimUtils } from "./array";
+
+const interpolateSearch = (targetTime: number, times: number[], allowExtrapolateRhs: boolean) => {
+  if (targetTime < times[0]) {
+    throw new Error(
+      `Tried to interpolate at time t = ${targetTime}, which is before first time (${times[0]})`
+    );
+  }
+
+  const n = times.length;
+
+  const upperTimeIdx = times.findIndex(t => targetTime <= t);
+  if (upperTimeIdx === -1) {
+    // target time greater than all times
+    if (allowExtrapolateRhs) {
+      return n - 1;
+    } else {
+      throw new Error(
+        `Tried to interpolate at time t = ${targetTime}, which is after last time (${times[n - 1]})`
+      )
+    }
+  }
+
+  return times[upperTimeIdx] !== targetTime ? upperTimeIdx - 1 : upperTimeIdx;
+}
+
+const validateTime = (times: number[], lengthY: number, nameT: string, nameY: string) => {
+  if (times.length !== lengthY) {
+    throw new Error(
+      `Time variable '${nameT}' and interpolation target '${nameY}' must have the same length, `
+      + `but do not (${times.length} vs ${lengthY})`
+    );
+  }
+
+  for (let i = 1; i < times.length; i++) {
+    if (times[i - 1] >= times[i]) {
+      throw new Error(
+        `Time variable '${nameT}' must be strictly increasing but was not at index ${i}`
+      );
+    }
+  }
+};
+
+class InterpolateConstant {
+  constructor(
+    public times: number[],
+    public y: number[],
+    nameT: string,
+    nameY: string,
+  ) {
+    validateTime(times, y.length, nameT, nameY);
+  };
+
+  eval = (t: number) => {
+    const idx = interpolateSearch(t, this.times, true);
+    return this.y[idx];
+  };
+}
+
+class InterpolateConstantArray {
+  constructor(
+    public times: number[],
+    public y: number[],
+    public dimTarget: DimUtils,
+    nameT: string,
+    nameY: string,
+  ) {
+    const lengthY = y.length / dimTarget.size;
+    validateTime(times, lengthY, nameT, nameY);
+  };
+
+  eval = (t: number) => {
+    const idx = interpolateSearch(t, this.times, true);
+    const size = this.dimTarget.size;
+    const startOffset = size * idx;
+    return this.y.slice(startOffset, startOffset + size);
+  };
+}
+
+export const interpolate = {
+  InterpolateConstant,
+  InterpolateConstantArray,
+};
